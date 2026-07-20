7760d2ab7e496aad67c58cfd69e111ef7b76bcba
Amirhosein Dalvand <amirhoseindalvand@gmail.com>
2026-07-20 11:10:58 +0200

    Add basic pass Economics exam

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const APP_VERSION = "20260720-latex-delimiters-v1";
+  const APP_VERSION = "20260720-basic-pass-exam-v1";
   const EXAM_CATALOG_URL = `./data/exams.json?v=${APP_VERSION}`;
   const LEGACY_STORAGE_KEY = "exam-sprint-state-v1";
   const STATE_KEY_PREFIX = "exam-sprint-state";
